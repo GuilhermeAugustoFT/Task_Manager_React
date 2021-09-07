825f6cfc25be7fb4712558aab77acaae932b7195
Guilherme Augusto <guilherme.augusto23@hotmail.com>
2021-09-07 15:21:49 -0300

    Correção de bugd

--- project/src/components/Tasks.jsx
+++ project/src/components/Tasks.jsx
@@ -4,7 +4,7 @@ import Task from "./Task";
 const Tasks = ({ tasks, handleTaskClick, handleTaskDeletion }) => {
     return <div>
         
-        {tasks.map(task => <Task task={task} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion}/>)}
+        {tasks.map(task => <Task key={task.id} task={task} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion}/>)}
 
     </div>
 }
